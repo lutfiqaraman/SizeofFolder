91402a7a48e81acc2245d102ce60cdbe433db981
lutfiqaraman <lutfiqaraman@gmail.com>
2018-05-05 15:29:29 +0300

    Done

--- SizeofFolder/Program.cs
+++ SizeofFolder/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,26 @@ namespace SizeofFolder
     {
         static void Main(string[] args)
         {
+            try
+            {
+                string path = "c:\\your\\path\\is\\here\\";
+                string[] files = Directory.GetFiles(path);
+                double size = files.Select(file => new FileInfo(file).Length).Sum();
+
+                size = Math.Round(size / 1000000, 1);
+
+                if (size >= 300)
+                    Console.WriteLine("The size of directory is {0} MB. \n Directory is overloaded", size);
+                else
+                    Console.WriteLine("The size of directory is {0} MB. \n Directory is not overloaded", size);
+            }
+
+            catch (DirectoryNotFoundException dirEx)
+            {
+                Console.WriteLine("Directory not found: " + dirEx.Message);
+            }
+
+            Console.ReadKey();
         }
     }
 }
